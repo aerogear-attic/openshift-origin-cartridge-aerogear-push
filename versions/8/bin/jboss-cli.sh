796b0e29dcbafad2a6c0b617d48294cc1ec856ff
Farah Juma <fjuma@redhat.com>
2014-04-16 13:05:21 -0400

    Updating to WildFly 8.1.0.CR1

--- versions/8/bin/jboss-cli.sh
+++ versions/8/bin/jboss-cli.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+CLI_OPTS=""
+while [ "$#" -gt 0 ]
+do
+    case "$1" in
+      *)
+          CLI_OPTS="$CLI_OPTS \"$1\""
+          ;;
+    esac
+    shift
+done
+
+
 DIRNAME=`dirname "$0"`
 
 # OS specific support (must be 'true' or 'false').
@@ -72,7 +84,14 @@ else
     JAVA_OPTS="$JAVA_OPTS -Djboss.modules.system.pkgs=com.sun.java.swing"
 fi
 
+LOG_CONF=`echo $JAVA_OPTS | grep "logging.configuration"`
+if [ "x$LOG_CONF" = "x" ]; then
+    JAVA_OPTS="$JAVA_OPTS \"-Dlogging.configuration=file:$JBOSS_HOME/bin/jboss-cli-logging.properties\""
+else
+    echo "logging.configuration already set in JAVA_OPTS"
+fi
+
 # Sample JPDA settings for remote socket debugging
 #JAVA_OPTS="$JAVA_OPTS -agentlib:jdwp=transport=dt_socket,address=8787,server=y,suspend=n"
 
-eval \"$JAVA\" $JAVA_OPTS \"-Dlogging.configuration=file:$JBOSS_HOME/bin/jboss-cli-logging.properties\" -jar \"$JBOSS_HOME/jboss-modules.jar\" -mp \"${JBOSS_MODULEPATH}\" org.jboss.as.cli '"$@"'
+eval \"$JAVA\" $JAVA_OPTS -jar \"$JBOSS_HOME/jboss-modules.jar\" -mp \"${JBOSS_MODULEPATH}\" org.jboss.as.cli "$CLI_OPTS"
